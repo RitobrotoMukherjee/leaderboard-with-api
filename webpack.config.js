8888755fad9ee91d0e7c5c9d97da6ac2f6da3bfc
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-15 15:03:46 +0530

    Add linters and html boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        filename: 'build.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Leader Board',
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leader Board',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
