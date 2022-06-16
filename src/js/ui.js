66daa184f385e1c4860c6b981dc7e4b5c63e2649
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-16 20:30:04 +0530

    Add styling to the page and made it responsive

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,7 @@
 const leaderList = document.getElementById('score-list');
 export default (leaderBoardList) => {
-  leaderList.innerHTML = leaderBoardList.map(({ user, score }) => `<li class="score">
-        <p>${user}: ${+score > 0 ? +score : 0}</p>
+  leaderList.innerHTML = leaderBoardList.map(({ user, score }, i) => `<li class="score score-items">
+        <div class="serial"><p>${i + 1}</p></div>
+        <div class="details"><p>${user}</p> <p>${+score > 0 ? +score : 0}</p></div>
     </li>`).join(' ');
 };
